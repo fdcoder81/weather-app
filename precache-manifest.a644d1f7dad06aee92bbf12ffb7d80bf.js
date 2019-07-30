463d1b2a857a7df89afc2843c677a0cd7679061f
Fdcoder81 <carloanselmi81@gmail.com>
2019-07-30 12:03:35 +0200

    Updates

---
+++ precache-manifest.a644d1f7dad06aee92bbf12ffb7d80bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30402e83a2421297ce1a9b0aa12277af",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "6463fb8dc5eda74db572",
+    "url": "/weather-app/static/css/main.e252e4ec.chunk.css"
+  },
+  {
+    "revision": "60fa97efd063c860df28",
+    "url": "/weather-app/static/js/2.88cff338.chunk.js"
+  },
+  {
+    "revision": "6463fb8dc5eda74db572",
+    "url": "/weather-app/static/js/main.ef8ff5dc.chunk.js"
+  },
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  },
+  {
+    "revision": "a1f22e2ebb461b9b4988b0be0460a530",
+    "url": "/weather-app/static/media/weather.a1f22e2e.jpg"
+  }
+]);
