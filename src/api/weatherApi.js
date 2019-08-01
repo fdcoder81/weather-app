c9e71910c80bf89bb3a422ea47f058123d378602
Fdcoder81 <carloanselmi81@gmail.com>
2019-08-01 10:55:58 +0200

    Git rebase fix

--- src/api/weatherApi.js
+++ src/api/weatherApi.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 export const weatherLocation = axios.create({
-  baseURL: "http://dataservice.accuweather.com/locations/v1"
+  baseURL: "https://dataservice.accuweather.com/locations/v1"
 });
 
 export const weatherData = axios.create({
-  baseURL: "http://dataservice.accuweather.com/currentconditions/v1/"
+  baseURL: "https://dataservice.accuweather.com/currentconditions/v1/"
 });
 
 export const weatherForecast = axios.create({
-  baseURL: "http://dataservice.accuweather.com/forecasts/v1/daily/5day/"
+  baseURL: "https://dataservice.accuweather.com/forecasts/v1/daily/5day/"
 });
